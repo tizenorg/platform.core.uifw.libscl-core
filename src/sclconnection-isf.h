55956c5311009d1b15b059a8db1a219d41bedcfa
Li Zhang <li2012.zhang@samsung.com>
2015-07-24 18:59:44 +0800

    Update ISE geometry to ISF
    Change-Id: Ifbdd45404f4141299f07fa8d1583a85aa864c4a2

--- src/sclconnection-isf.h
+++ src/sclconnection-isf.h
@@ -78,6 +78,7 @@ public:
     void candidate_hide(void);
     void set_keyboard_ise_by_uuid(const sclchar *uuid);
     void get_keyboard_ise(const sclchar *uuid);
+    void update_geometry(int x, int y, int width, int height);
 
 private:
     sclboolean m_initialized;
